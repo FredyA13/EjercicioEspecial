bfcc2166778ad59d961761ff5e97654f0e410e52
FredyA13 <fredy.alonzo2001@hotmail.com>
2025-05-02 23:33:26 -0600

    Se termina la Parte 3

--- html/js/main.js
+++ html/js/main.js
@@ -12,7 +12,9 @@ btnAgregar.addEventListener("click", function(event){
                     console.log(response);
 
                     response.json().then((res) =>{
-                        createCards(res);
+                        console.log(res[0].title);
+                        
+                        //createCards(res);
                     });
 
                 })
@@ -23,14 +25,15 @@ btnAgregar.addEventListener("click", function(event){
 function createCards(prods){
 
     for (let i = 0; i < prods.length; i++) {
-        const prods = prods[i];
+        const producto = prods[i];
 
         mainProds.insertAdjacentHTML("beforeend",
             `<div class="col">
           <div class="card shadow-sm">
             <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
             <div class="card-body">
-              <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+              <h5>${producto.title}</h5>
+              <p class="card-text"></p>
               <div class="d-flex justify-content-between align-items-center">
                 <div class="btn-group">
                   <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
