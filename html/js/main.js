6f62b1f51f16d59c2cad677a6bee137b3ba59a33
FredyA13 <fredy.alonzo2001@hotmail.com>
2025-05-05 00:54:32 -0600

    Se completa el ejercicio

--- html/js/main.js
+++ html/js/main.js
@@ -2,48 +2,59 @@ const URLMain = "https://api.escuelajs.co/api/v1/products";
 const btnAgregar = document.getElementById("btnAgregar");
 const mainProds = document.getElementById("mainProds");
 
-btnAgregar.addEventListener("click", function(event){
-    event.preventDefault();
+btnAgregar.addEventListener("click", function (event) {
+  event.preventDefault();
 
-    const options = {"method": "GET"};
+  const options = { "method": "GET" };
 
-    fetch(URLMain, options)
-                .then((response) =>{
-                    console.log(response);
+  fetch(URLMain, options)
+    .then((response) => {
+      console.log(response);
 
-                    response.json().then((res) =>{
-                        console.log(res[0].title);
-                        
-                        //createCards(res);
-                    });
+      response.json().then((res) => {
+        console.log(res[0].title);
 
-                })
-                .catch()
+        modifyCards(res);
+      });
+
+    })
+    .catch();
 
 });//btnAgregar
 
-function createCards(prods){
+function modifyCards(prods) {
+  const cards = document.querySelectorAll("#mainProds .col"); // todas las tarjetas
 
-    for (let i = 0; i < prods.length; i++) {
-        const producto = prods[i];
+  for (let i = 0; i < cards.length && i < prods.length; i++) {
 
-        mainProds.insertAdjacentHTML("beforeend",
-            `<div class="col">
-          <div class="card shadow-sm">
-            <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
-            <div class="card-body">
-              <h5>${producto.title}</h5>
-              <p class="card-text"></p>
-              <div class="d-flex justify-content-between align-items-center">
-                <div class="btn-group">
-                  <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
-                  <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
-                </div>
-                <small class="text-body-secondary">$ price</small>
-              </div>
-            </div>
-          </div>
-        </div>`
-        );
-    }
-}//CreateCards
+    const producto = prods[i];
+    const card = cards[i];
+
+    //Modificamos la imagen
+    const img = card.querySelector("svg");
+    const newImg = document.createElement("img");
+    newImg.src = producto.images[1];
+    newImg.className = "card-img-top";
+    newImg.alt = producto.title;
+    newImg.width = "100%";
+    newImg.height = 225;
+    newImg.setAttribute("referrerpolicy", "no-referrer");
+    img.replaceWith(newImg);
+
+    //Agregamos el titulo
+    const titulo = card.querySelector(".card-body");
+    titulo.insertAdjacentHTML("afterbegin",
+      `<p style="font-weight: bold;">${producto.title}</p>`
+    );
+
+    //Modificamos la descripcion
+    const descripcion = card.querySelector(".card-text");
+    descripcion.innerText = producto.description;
+
+    //Modificamos los precios
+    const precios = card.querySelector(".text-body-secondary");
+    precios.innerText = "$" + producto.price;
+
+
+  }
+}
